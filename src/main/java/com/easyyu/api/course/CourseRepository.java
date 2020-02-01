9d9b3aad4ee8925b5ff671fb5c18e36b8aa276ea
jackehuynh <jackie.huynh@hotmail.com>
2020-02-01 16:13:59 -0500

    implemented additional queries

--- src/main/java/com/easyyu/api/course/CourseRepository.java
+++ src/main/java/com/easyyu/api/course/CourseRepository.java
@@ -1,14 +1,16 @@
 package com.easyyu.api.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
-    boolean existsCourseByFacultyAndSubjectAndNameAndCourse_numberAndCredit(String faculty,
-                                                                            String subject,
-                                                                            String courseName,
-                                                                            String courseNumber,
-                                                                            String credit);
-
+    List<Course> findBySubjectAllIgnoreCase(String subject);
+    List<Course> findBySubjectAndCourseNumberAllIgnoreCase(String subject, String courseNumber);
+    List<Course> findByFacultyAllIgnoreCase(String faculty);
+    List<Course> findByFacultyAndSubjectAndCourseNumberAllIgnoreCase(
+            String faculty, String Subject, String courseNumber);
 }
