ca7611098a2262b1ce7a05e40e66a16db58da136
jackehuynh <jackie.huynh@hotmail.com>
2020-02-01 16:17:16 -0500

    switched to using service layer instead of repository
    In addition:
    - implemented support for handling of json and jsonb columns
    - refactored some variable names to Java's naming convention

--- src/main/java/com/easyyu/api/course/Course.java
+++ src/main/java/com/easyyu/api/course/Course.java
@@ -1,21 +1,90 @@
 package com.easyyu.api.course;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.easyyu.api.course.offerings.CourseOfferingInfo;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@TypeDefs({
+        @TypeDef(name = "json", typeClass = JsonStringType.class),
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
 @Entity
 @Table(name = "courses")
-public class Course {
+@JsonPropertyOrder(
+        { "faculty", "subject", "course_number", "name", "description", "credit",
+        "instruction_language", "offerings", "url"})
+public class Course implements Serializable {
 
     private String faculty;
     private String subject;
-    private String courseCode;
     private String name;
     private String description;
     private String credit;
-    private String loi;
 
-    private String url;
+    @Column(name = "course_number")
+    @JsonProperty(value = "course_number")
+    private String courseNumber;
+
+    @Column(name = "loi")
+    private String instruction_language;
+
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb", name = "offerings")
+    @Basic(fetch = FetchType.LAZY)
+    private List<CourseOfferingInfo> offerings;
+
+    @Id
+    @Column(name = "url")
+    private String course_url;
+
+    public void setOfferings(List<CourseOfferingInfo> offerings) {
+        this.offerings = offerings;
+    }
+
+    public List<CourseOfferingInfo> getOfferings() {
+        return offerings;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setcourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCredit(String credit) {
+        this.credit = credit;
+    }
+
+    public void setinstruction_language(String instruction_language) {
+        this.instruction_language = instruction_language;
+    }
+
+    public void setCourse_url(String url) {
+        this.course_url = url;
+    }
 
     public String getDescription() {
         return description;
@@ -23,6 +92,14 @@ public class Course {
 
     public String getCredit() {
         return credit;
+    }
+
+    public String getCourse_url() {
+        return course_url;
+    }
+
+    public String getinstruction_language() {
+        return instruction_language;
     }
 
     public String getName() {
@@ -37,7 +114,8 @@ public class Course {
         return subject;
     }
 
-    public String getCourseCode() {
-        return courseCode;
+    public String getcourseNumber() {
+        return courseNumber;
     }
+
 }
