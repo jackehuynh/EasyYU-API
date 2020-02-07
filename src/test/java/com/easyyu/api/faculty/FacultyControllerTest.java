4d550c4c7285405a4bc881c042cec21f62a16b6e
jackehuynh <jackie.huynh@hotmail.com>
2020-02-07 17:39:33 -0500

    added test for faculty controller

---
+++ src/test/java/com/easyyu/api/faculty/FacultyControllerTest.java
@@ -0,0 +1,59 @@
+package com.easyyu.api.faculty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(FacultyControllerTest.class)
+public class FacultyControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private FacultyController facultyController;
+
+    @Test
+    public void getSpecificFaculty() throws Exception {
+        Faculty testFaculty = new Faculty();
+        testFaculty.setCode("LE");
+        testFaculty.setName("Lassonde School of Engineering");
+
+        given(facultyController.getFaculty("LE")).willReturn(testFaculty);
+
+        mvc.perform(get("http://localhost:8080/api/v1/faculty/LE")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code", is(testFaculty.getCode())))
+                .andExpect(MockMvcResultMatchers.jsonPath("name", is(testFaculty.getName())));
+    }
+
+    @Test
+    // should return the same list size of all available faculty obtained from repo layer
+    public void getAllFaculty() throws Exception {
+        List<Faculty> faculty = facultyController.getAllFaculty();
+
+        given(facultyController.getAllFaculty()).willReturn(faculty);
+
+        mvc.perform(get("http://localhost:8080/api/v1/faculty")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(faculty.size())));
+    }
+}
